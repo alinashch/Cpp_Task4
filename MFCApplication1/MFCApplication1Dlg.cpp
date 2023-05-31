e7e448146e538d528d20e59198bc50da958beedb
alinashch <83130878+alinashch@users.noreply.github.com>
2023-05-31 22:11:23 +0300

    main

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,269 @@
+﻿
+// MFCApplication1Dlg.cpp: файл реализации
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+#include <iostream>
+
+#include <string> 
+
+#include <fstream>
+
+#include <list>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#include <vector>
+
+using namespace std;
+
+
+
+// Диалоговое окно CAboutDlg используется для описания сведений о приложении
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+// Реализация
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Диалоговое окно CMFCApplication1Dlg
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT1, _output);
+	DDX_Control(pDX, IDC_EDIT2, _input);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CMFCApplication1Dlg::OnBnClickedOk)
+	ON_EN_CHANGE(IDC_EDIT1, &CMFCApplication1Dlg::OnEnChangeEdit1)
+END_MESSAGE_MAP()
+
+
+// Обработчики сообщений CMFCApplication1Dlg
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Добавление пункта "О программе..." в системное меню.
+
+	// IDM_ABOUTBOX должен быть в пределах системной команды.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Задает значок для этого диалогового окна.  Среда делает это автоматически,
+	//  если главное окно приложения не является диалоговым
+	SetIcon(m_hIcon, TRUE);			// Крупный значок
+	SetIcon(m_hIcon, FALSE);		// Мелкий значок
+
+	// TODO: добавьте дополнительную инициализацию
+
+	return TRUE;  // возврат значения TRUE, если фокус не передан элементу управления
+}
+
+void CMFCApplication1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// При добавлении кнопки свертывания в диалоговое окно нужно воспользоваться приведенным ниже кодом,
+//  чтобы нарисовать значок.  Для приложений MFC, использующих модель документов или представлений,
+//  это автоматически выполняется рабочей областью.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // контекст устройства для рисования
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Выравнивание значка по центру клиентского прямоугольника
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Нарисуйте значок
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// Система вызывает эту функцию для получения отображения курсора при перемещении
+//  свернутого окна.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void tokenize(std::string const& str, const char delim,
+	std::vector<std::string>& out)
+{
+	size_t start;
+	size_t end = 0;
+
+	while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+	{
+		end = str.find(delim, start);
+		out.push_back(str.substr(start, end - start));
+	}
+}
+
+bool fun(CString out, CString inp) {
+	if (out.Right(4) != ".txt") {
+		return false;
+	}
+
+	if (inp.Right(4) != ".txt") {
+		return false;
+	}
+
+	std::string line;
+	string s = "";
+	std::list < string> stInput{  };
+
+	std::ifstream in(inp);
+	if (in.is_open())
+	{
+		while (std::getline(in, line))
+		{
+			stInput.push_back(line);
+		}
+	}
+
+	std::list < string> stSyn{  };
+
+	std::ifstream syn("syn.txt");
+	if (syn.is_open())
+	{
+		while (std::getline(syn, line))
+		{
+			stSyn.push_back(line);
+		}
+	}
+
+	int k = 0;
+	std::list < string> result{  };
+
+	for (string i : stInput) {
+
+		auto l_front = stSyn.begin();
+		std::advance(l_front, k);
+		string v = *l_front;
+		const char delim = ';';
+
+		std::vector<std::string> o;
+		tokenize(v, delim, o);
+		int t = o.size() ;
+		int ran = rand() % t + 0;
+		result.push_back(o[ran]);
+
+		k++;
+	}
+
+
+	ofstream fout(out);
+	for (string i : result) {
+		fout << i << std::endl;
+	}
+	fout.close();
+	return true;
+
+
+}
+
+void CMFCApplication1Dlg::OnBnClickedOk()
+{
+	CString out;
+	_output.GetWindowTextW(out);
+
+	CString inp;
+	_input.GetWindowTextW(inp);
+	
+	if (fun(out, inp)) {
+		AfxMessageBox(L"Correct");
+		CDialogEx::OnOK();
+	}
+	else {
+		AfxMessageBox(L"Error!!!!!!!!!");
+		CDialogEx::OnOK();
+	}
+}
+
+
+void CMFCApplication1Dlg::OnEnChangeEdit1()
+{
+
+}
